78e1620396409b450449bd4f94dc39c4ce53e9d1
Papop Lekhapanyaporn <75293379+Retaehc-pop@users.noreply.github.com>
2021-07-22 11:33:43 +0700

    Create _global.py

---
+++ settings_files/_global.py
@@ -0,0 +1,10 @@
+import os
+
+SETTINGS_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(SETTINGS_DIR)
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+
+DISCORD_BOT_TOKEN = "BOTTOKEN"
+
+REDDIT_APP_ID = "REDDIT_ADD_ID"
+REDDIT_APP_SECRET = "REDDIT_SECRET_ID"
