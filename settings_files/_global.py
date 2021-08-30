016100e78bcffa758e758e0f017364a75bbc1245
Papop Lekhapanyaporn <75293379+Retaehc-pop@users.noreply.github.com>
2021-08-30 23:47:33 +0700

    commit main

--- settings_files/_global.py
+++ settings_files/_global.py
@@ -4,9 +4,18 @@ SETTINGS_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(SETTINGS_DIR)
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
 
-DISCORD_BOT_TOKEN = "BOTTOKEN"
 
-REDDIT_APP_ID = "REDDIT_ADD_ID"
-REDDIT_APP_SECRET = "REDDIT_SECRET_ID"
+DISCORD_BOT_TOKEN = "ODY2MTg0Nzg5ODExMzk2NjQ5.YPO3cg.tjbcL28Jw_g_TT7hhA_fsAMSfyY"
+
+REDDIT_APP_ID = "BU7hiKqPsykKA92llIH1OQ"
+REDDIT_APP_SECRET = "qmLjnJ6m5gceOogoiRQR2byc3ehr1w"
+
+REDDIT_ENABLED_MEME_SUBREDDITS = [
+    'funny',
+    'memes',
+]
+REDDIT_ENABLED_NSFW_SUBREDDITS = [
+    'wtf'
+]
 
 MODERATOR_ROLE_NAME = "Moderator"
